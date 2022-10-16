77e79fd4b96c2f157e7174b57e4e1ac5f143c77c
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2022-10-16 23:53:31 +0200

    Add files via upload

---
+++ klasaB/klasaB/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaB
+{
+    class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string bpiz)
+        {
+            bpiz = bpiz.Remove(bpiz.Length - 1, 1);
+            bpiz = bpiz.Remove(0, 1);
+            return bpiz;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaB klasaB = new KlasaB();
+            Console.WriteLine("Unesi niz znakova: ");
+            string s = Console.ReadLine();
+            Console.WriteLine(klasaB.BezPrvogIZadnjeg(s));
+            Console.ReadKey();
+        }
+    }
+}
